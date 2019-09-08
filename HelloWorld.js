5bc057ecf4950d5a2b81aef9248c690596963217
Miriam Ramirez <miriam.ramirez@unosquare.com>
2019-09-07 23:06:10 -0500

    change2 on 1st branch

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,4 @@
 // Exercise 1 - Git and GitHub 
-console.log("Hello World"); 
+console.log("Hello World Exercise 1 - Git and GitHub"); 
 console.log("Changes first branch")
+console.log("Change 2 first branch")
