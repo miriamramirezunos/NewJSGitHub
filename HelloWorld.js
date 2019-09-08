1be7c34d7bb3f89275a1b44de4e988db6f8d3095
Miriam Ramirez <miriam.ramirez@unosquare.com>
2019-09-07 23:41:57 -0500

    change2 -> pull request

--- HelloWorld.js
+++ HelloWorld.js
@@ -3,3 +3,4 @@ console.log("Hello World Exercise 1 - Git and GitHub");
 console.log("Changes first branch")
 console.log("Change 2 first branch")
 console.log("change 1 -> pull request ")
+console.log("change 2 -> pull request ")
